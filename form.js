e9ac214fc9bf724131495a8310379818ef5da17f
semon-sahu <semonsahu111222@gmail.com>
2024-10-03 23:53:02 +0530

    js array

---
+++ form.js
@@ -0,0 +1,12 @@
+document.getElementById("btn1").addEventListener("click",datasave);
+document.getElementById("btn2").addEventListener("click",dataDisplay);
+function dataSave(){
+    let rollno=document.getElementById("rno").value;
+    let name=document.getElementById("nm").value;
+    let city=document.getElementById("ct").value;
+    let fees=document.getElementById("fs").value;
+
+    let api=""
+
+
+}
